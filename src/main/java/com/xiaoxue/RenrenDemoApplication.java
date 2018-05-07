48e2d802abf559b9f66d3533d97973f02b155e7d
xueheng <>
2018-05-07 14:03:29 +0800

    Initial commit

---
+++ src/main/java/com/xiaoxue/RenrenDemoApplication.java
@@ -0,0 +1,12 @@
+package com.xiaoxue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenrenDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RenrenDemoApplication.class, args);
+    }
+}
---
+++ src/test/java/com/xiaoxue/RenrenDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xiaoxue;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RenrenDemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
