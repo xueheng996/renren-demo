2a2b5316a2f097c923ef31e2452e8275a8e1a275
xueheng <xuehheng@163.com>
2018-08-07 23:34:08 +0800

    权限管理

--- src/main/java/com/xiaoxue/modules/sys/service/SysUserRoleService.java
+++ src/main/java/com/xiaoxue/modules/sys/service/SysUserRoleService.java
@@ -4,12 +4,18 @@ package com.xiaoxue.modules.sys.service;
 import com.baomidou.mybatisplus.service.IService;
 import com.xiaoxue.common.utils.PageUtils;
 import com.xiaoxue.modules.sys.entity.SysUserEntity;
+import com.xiaoxue.modules.sys.entity.SysUserRoleEntity;
 
+import java.util.List;
 import java.util.Map;
 
 
-public interface SysUserRoleService extends IService<SysUserEntity> {
+public interface SysUserRoleService extends IService<SysUserRoleEntity> {
 
-    PageUtils queryPage(Map<String, Object> params);
+     void saveOrUpdate(Long userId, List<Long>roleIdList);
+
+     List<Long> queryRoleIdList(Long userId);
+
+     int deleteBatch(Long[] roleIds);
 
 }
--- src/main/java/com/xiaoxue/modules/sys/service/impl/SysUserRoleServiceImpl.java
+++ src/main/java/com/xiaoxue/modules/sys/service/impl/SysUserRoleServiceImpl.java
@@ -5,22 +5,32 @@ import com.baomidou.mybatisplus.plugins.Page;
 import com.baomidou.mybatisplus.service.impl.ServiceImpl;
 import com.xiaoxue.common.utils.PageUtils;
 import com.xiaoxue.modules.sys.dao.SysUserDao;
+import com.xiaoxue.modules.sys.dao.SysUserRoleDao;
 import com.xiaoxue.modules.sys.entity.SysUserEntity;
+import com.xiaoxue.modules.sys.entity.SysUserRoleEntity;
+import com.xiaoxue.modules.sys.service.SysUserRoleService;
 import com.xiaoxue.modules.sys.service.SysUserService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Map;
 
 @Service
-public class SysUserRoleServiceImpl extends ServiceImpl<SysUserDao, SysUserEntity> implements SysUserService {
+public class SysUserRoleServiceImpl extends ServiceImpl<SysUserRoleDao, SysUserRoleEntity> implements SysUserRoleService {
 
 
     @Override
-    public PageUtils queryPage(Map<String, Object> params) {
-        String username = (String) params.get("username");
+    public void saveOrUpdate(Long userId, List<Long> roleIdList) {
 
-        Page<SysUserEntity> page = this.selectPage(new Page<>(1, 10));
+    }
 
-        return new PageUtils(page);
+    @Override
+    public List<Long> queryRoleIdList(Long userId) {
+        return null;
+    }
+
+    @Override
+    public int deleteBatch(Long[] roleIds) {
+        return 0;
     }
 }
