5553c4d5ba4cd6eaf1c041e9cd3da365aeafe383
xueheng <xuehheng@163.com>
2018-08-13 07:13:28 +0800

    权限管理

--- src/main/java/com/xiaoxue/modules/sys/dao/SysUserDao.java
+++ src/main/java/com/xiaoxue/modules/sys/dao/SysUserDao.java
@@ -3,8 +3,10 @@ package com.xiaoxue.modules.sys.dao;
 import com.baomidou.mybatisplus.mapper.BaseMapper;
 import com.xiaoxue.modules.sys.entity.SysUserEntity;
 
+import java.util.List;
+
 
 public interface SysUserDao extends BaseMapper<SysUserEntity> {
 
-
+    List<String> queryAllPerms(Long userId);
 }
--- src/main/java/com/xiaoxue/modules/sys/shiro/UserRealm.java
+++ src/main/java/com/xiaoxue/modules/sys/shiro/UserRealm.java
@@ -1,18 +1,21 @@
 package com.xiaoxue.modules.sys.shiro;
 
 import com.xiaoxue.common.utils.Contant;
+import com.xiaoxue.modules.sys.dao.SysMenuDao;
 import com.xiaoxue.modules.sys.dao.SysUserDao;
 import com.xiaoxue.modules.sys.entity.SysMenuEntity;
 import com.xiaoxue.modules.sys.entity.SysUserEntity;
+import org.apache.commons.lang.StringUtils;
 import org.apache.shiro.authc.*;
 import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
 import org.apache.shiro.util.ByteSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
+import java.util.*;
 
 @Component
 public class UserRealm extends AuthorizingRealm {
@@ -20,16 +23,34 @@ public class UserRealm extends AuthorizingRealm {
     @Autowired
     private SysUserDao sysUserDao;
     @Autowired
-    private SysUserDao sysMenuDao;
+    private SysMenuDao sysMenuDao;
     @Override
     protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
         SysUserEntity userEntity= (SysUserEntity) principalCollection.getPrimaryPrincipal();
         Long userId=userEntity.getUserId();
-        if(userId== Contant.SUPER_ADMIN){
-           // List<SysMenuEntity> menuList=sysMenuDao.selectList(null);
 
+        List<String> permsList;
+
+        if(userId== Contant.SUPER_ADMIN){
+            List<SysMenuEntity> menuList=sysMenuDao.selectList(null);
+            permsList=new ArrayList<>(menuList.size());
+            for (SysMenuEntity menu:menuList){
+                permsList.add(menu.getPerms());
+            }
+        }else {
+            permsList=sysUserDao.queryAllPerms(userId);
         }
-        return null;
+        //用户权限列表
+        Set<String > permsSet=new HashSet<>();
+        for (String perms:permsList){
+            if(StringUtils.isBlank(perms)){
+                continue;
+            }
+            permsSet.addAll(Arrays.asList(perms.trim().split(",")));
+        }
+        SimpleAuthorizationInfo info=new SimpleAuthorizationInfo();
+        info.setStringPermissions(permsSet);
+        return info;
     }
 
     @Override
